b2a8e81972f45cebc62e76e0c3017d1b092b8228
Nickan Nazari <nickannazari@yahoo.com>
2021-04-19 14:15:45 -0700

    Changed circle

---
+++ circle.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+//This program prompts user to enter circle raduis
+//Then the circle area is displayed
+
+int main()
+{
+	const double TWOPI = 3.14159 * 2;
+	double radius;
+	cout << "Enter radius: ";
+	cin >> radius;
+
+	//Calculate the area of the circle
+	double area = TWOPI * pow(radius, 2);
+	cout << "The area of a circle with radius " << radius << " is " << area << endl;
+	return 0;
+}
